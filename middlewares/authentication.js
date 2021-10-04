c3f9a823486441ec35a6c587c48c9323134ca63a
Tanya Kalkuta <Tanyakalkuta@gmail.com>
2021-10-05 01:50:26 +0300

    Update authentication

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,5 +1,29 @@
+const jwt = require('jsonwebtoken');
+const { Unauthorized } = require('http-errors');
+
+const { User } = require('../model/schemas');
+
+const { SECRET_KEY } = process.env;
+
 const authentication = async (req, res, next) => {
-  next();
+  try {
+    const [bearer, token] = req.headers.authorization.split(' ');
+    if (bearer !== 'Bearer') {
+      throw new Unauthorized('Not authorized');
+    }
+    const { id } = jwt.verify(token, SECRET_KEY);
+    const userId = await User.findById(id);
+
+    const user = await User.findOne({ token });
+    if (!user || !userId) {
+      throw new Unauthorized('Not authorized');
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new Unauthorized('Not authorized');
+  }
 };
 
 module.exports = authentication;
