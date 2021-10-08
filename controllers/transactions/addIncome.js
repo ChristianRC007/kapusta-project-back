1c19c98ba9e80a8ddab28cae212d5357f760b7c9
Dmitriy Nikolaiev <dimnik.u11@gmail.com>
2021-10-08 13:56:16 +0300

    Добавляет обновление баланса при транзакции addIncome

--- controllers/transactions/addIncome.js
+++ controllers/transactions/addIncome.js
@@ -1,16 +1,24 @@
 const { Transaction } = require('../../model/schemas');
+const { User } = require('../../model/schemas');
 
 const addIncome = async (req, res, next) => {
   try {
+    const { amount } = req.body;
+    const userId = req.user._id.toString();
+
     const transactionData = await Transaction.create({
       ...req.body,
       isIncome: true,
-      // owner: req.user._id,
+      owner: userId,
     });
 
     res.status(201).json({
       transactionData,
     });
+
+    const user = await User.findById(userId);
+    user.balance += amount;
+    await user.save();
   } catch (error) {
     next(error);
   }
