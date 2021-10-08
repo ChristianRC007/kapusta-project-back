6823e2b65981ee1dd964e66e4a88f3f301545b86
Tanya Kalkuta <Tanyakalkuta@gmail.com>
2021-10-08 21:21:59 +0300

    updateBalance

--- controllers/users/updateBalance.js
+++ controllers/users/updateBalance.js
@@ -1,6 +1,29 @@
-// const { User } = require('../../model/schemas');
+const { User } = require('../../model/schemas');
+const { NotFound } = require('http-errors');
 
-const updateBalance = async () => {
+const updateBalance = async (req, res, next) => {
+  try {
+    const { _id } = req.user._id;
+    const { balance } = req.body;
+    const updateUser = await User.findByIdAndUpdate(
+      _id,
+      { balance },
+      { new: true },
+    );
+    if (!updateUser) {
+      throw new NotFound();
+    }
+    res.json({
+      status: 'success',
+      code: 200,
+      user: {
+        email: updateUser.email,
+        balance: updateUser.balance,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
   console.log('updateBalance');
 };
 
