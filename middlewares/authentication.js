6861bbfed7069bec738e504bd70528e9a2f1d5ee
Dmitriy Nikolaiev <dimnik.u11@gmail.com>
2021-10-08 13:54:14 +0300

    Добавляет endpoint login

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -3,19 +3,26 @@ const { Unauthorized } = require('http-errors');
 
 const { User } = require('../model/schemas');
 
+require('dotenv').config();
 const { SECRET_KEY } = process.env;
 
 const authentication = async (req, res, next) => {
   try {
+    if (!req.headers.authorization) {
+      throw new Unauthorized();
+    }
+
     const [bearer, token] = req.headers.authorization.split(' ');
     if (bearer !== 'Bearer') {
       throw new Unauthorized('Not authorized');
     }
-    const { id } = jwt.verify(token, SECRET_KEY);
-    const userId = await User.findById(id);
+    // const { id } = jwt.verify(token, SECRET_KEY);
+    // const userId = await User.findById(id);
+    jwt.verify(token, SECRET_KEY);
 
     const user = await User.findOne({ token });
-    if (!user || !userId) {
+    // if (!user || !userId) {
+    if (!user) {
       throw new Unauthorized('Not authorized');
     }
 
