2381011a7808c4f3d30366954faf849e6e15b67e
Dmitriy Nikolaiev <dimnik.u11@gmail.com>
2021-10-10 01:24:11 +0300

    Добавляет эндпоин получения сводки

--- controllers/transactions/getExpenseByDate.js
+++ controllers/transactions/getExpenseByDate.js
@@ -10,7 +10,7 @@ const getExpenseByDate = async (req, res, next) => {
       owner: req.user._id,
     }).populate('owner', '_id email name balance');
 
-    res.status(201).json({
+    res.status(200).json({
       transactions,
     });
   } catch (error) {
